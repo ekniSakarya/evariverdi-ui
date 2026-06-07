af698945de6f006aafd6048e114dd36e8a7341e3
ekniSakarya <eknisakarya@icloud.com>
2026-06-07 21:44:13 +0300

    first test page

---
+++ app/src/pages/Home.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen w-full bg-blue-600 flex items-center justify-center">
+      <h1 className="text-white text-9xl font-bold tracking-wider">VOX</h1>
+    </div>
+  )
+}
