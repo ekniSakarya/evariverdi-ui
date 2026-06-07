de04b6bb094db11a60588f29b78fd71fd640d970
ekniSakarya <eknisakarya@icloud.com>
2026-06-08 00:30:06 +0300

    .

--- app/src/pages/Home.tsx
+++ app/src/pages/Home.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
-    <div className="min-h-screen w-full bg-blue-600 flex items-center justify-center">
-      <h1 className="text-white text-9xl font-bold tracking-wider font-playfair">
-        VOX
-      </h1>
-    </div>
+   <div style={{ backgroundImage: "url('/vox-main.png')" }} className="bg-cover bg-center">
+     <div className="bg-black/40">  {/* koyu overlay */}
+       <h1 className="font-playfair text-9xl">VOX</h1>
+     </div>
+   </div>
   );
 }
