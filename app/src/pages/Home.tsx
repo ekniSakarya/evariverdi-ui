3c9c1ce35053bf31ab7b74478765d8c075387fe8
ekniSakarya <eknisakarya@icloud.com>
2026-06-07 23:25:56 +0300

    font

--- app/src/pages/Home.tsx
+++ app/src/pages/Home.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <div className="min-h-screen w-full bg-blue-600 flex items-center justify-center">
-      <h1 className="text-white text-9xl font-bold tracking-wider">VOX</h1>
+      <h1 className="text-white text-9xl font-bold tracking-wider font-playfair">
+        VOX
+      </h1>
     </div>
-  )
+  );
 }
